6aea3b0b0c7010e96683a2160eca46b300770617
Fernando Molano <fm87usa@gmail.com>
2022-03-23 22:43:05 -0400

    [UPDATE] navbar for medium and large sizes, toggle burger menu on mobile

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./styles/App.css";
 function App() {
   return (
     <div className="App">
-      <div className="container max-w-screen-lg mx-auto h-screen">
+      <div className="container max-w-screen-lg mx-auto h-screen px-3">
         <Header />
         <div className="text-left text-5xl font-extrabold">
           <span className="bg-clip-text text-transparent bg-gradient-to-br from-blue-400 to-emerald-500">
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,15 @@ function Header() {
   const [mobileMenu, setMobileMenu] = useState(false);
 
   return (
-    <nav className="w-full flex justify-between items-center px-3 h-24 text-white">
+    <nav className="w-full flex justify-between items-center h-24 text-white">
       <div className="text-2xl font-extrabold">molcadev</div>
       <div
-        className="text-lg text-slate-300 burger-menu"
+        className="text-lg text-slate-300"
         onClick={() => setMobileMenu(!mobileMenu)}
       >
-        <CgMenuMotion size={28} />
+        <CgMenuMotion size={32} className="sm:hidden" />
         {mobileMenu && (
-          <div className="mobile-menu absolute top-20 right-0.5 bg-slate-700 w-full h-96 flex items-center">
+          <div className="absolute top-20 right-0.5 bg-slate-700 w-full h-96 flex items-center">
             <ul className="flex flex-col h-full mx-auto items-start justify-evenly">
               <li>
                 <span className="font-bold text-white">01</span>Home
@@ -29,6 +29,22 @@ function Header() {
               </li>
             </ul>
           </div>
+        )}
+        {!mobileMenu && (
+          <ul className="hidden sm:flex items-center space-x-8">
+            <li>
+              <span className="font-bold text-white">01</span>Home
+            </li>
+            <li>
+              <span className="font-bold text-white">02</span>Work
+            </li>
+            <li>
+              <span className="font-bold text-white">03</span>About me
+            </li>
+            <li>
+              <span className="font-bold text-white">04</span>Contact
+            </li>
+          </ul>
         )}
       </div>
     </nav>
