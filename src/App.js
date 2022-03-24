a657005b2639cdd667c422eab8cbb33223a7aeca
Fernando Molano <fm87usa@gmail.com>
2022-03-23 21:10:50 -0400

    [ADD] background color, test tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./styles/App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div class="w-full text-5xl font-extrabold">
+        <span class="bg-clip-text text-transparent bg-gradient-to-br from-blue-400 to-emerald-500">
+          Fernando Molano
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default App;
